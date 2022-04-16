d67ee5dd342dd77e5c95ddc731f0e31ddd28e93a
FraustoB <brandon.frausto18@gmail.com>
2022-04-15 21:03:14 -0500

    included a UUID library to solve react-dom-error warning where it suggested a unique KEY for each items in a list that was created by the MAP function. Adjusted screen sized for mobile users that ensures app still works at current stage of the build

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BaseContainer } from "./components/baseContainer";
 
 function App() {
   return (
-    <div className="flex items-center justify-center flex-col bg-stone-800 text-white h-screen">
+    <div className="flex items-center justify-center flex-col bg-stone-800 text-white h-screen w-auto">
       <div className="flex flex-col items-center bg-stone-600 rounded-lg  ">
-        <h1 className="text-3xl font-bold underline mt-4 mb-4">
+        <h1 className="text-lg font-bold underline mt-4 mb-2">
           KILO-CALCULATOR
         </h1>
         <BaseContainer />
