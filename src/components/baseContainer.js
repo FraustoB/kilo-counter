c70a3d42515111a194ab1f0e512ababc7ba6c66a
FraustoB <brandon.frausto18@gmail.com>
2022-04-17 21:43:11 -0500

    added reducer functions and global state for KILO amount to show along side LBS on the main page Adjusted functions in plateAdder.js and currentPLates.js to accomodate for these changes.Adjusted margins for Kilo Plate Holder

--- src/components/baseContainer.js
+++ src/components/baseContainer.js
@@ -7,15 +7,17 @@ import { CurrentPlates } from "./currentPlates";
 export function BaseContainer() {
   // const curPlate = useSelector((state) => state.plates.value.plate);
   const curAmount = useSelector((state) => state.counter.value);
+  const curKiloAmount = useSelector((state) => state.counter.kiloValue);
 
   return (
     <div className=" w-auto lg:w-3/4">
       <div className="flex flex-col justify-center items-center ">
-        <span className="font-semibold text-base">Current Weight (lbs)</span>
+        <span className="font-semibold text-base">Current Weight</span>
+        <span className="text-lg font-medium">{curKiloAmount} kg</span>
+        <span className="text-lg font-medium">
+          {Math.floor(curAmount * 100) / 100} lbs
+        </span>
         <CurrentPlates />
-        <br />
-        {Math.floor(curAmount * 100) / 100}
-        <br />
         <PlateAdder />
       </div>
     </div>
