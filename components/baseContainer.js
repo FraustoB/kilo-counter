78b2063a9fdbfd9278840d2ba1ad801b413b71c0
FraustoB <brandon.frausto18@gmail.com>
2022-04-18 20:06:42 -0500

    adding CSS stylings for better UI for desktop/ipad users and better UI for mobile Users

--- components/baseContainer.js
+++ components/baseContainer.js
@@ -12,7 +12,7 @@ export function BaseContainer() {
   return (
     <div className=" w-auto lg:w-3/4">
       <div className="flex flex-col justify-center items-center ">
-        <span className="font-semibold text-base">Current Weight</span>
+        <span className="font-semibold mt-5 text-xl">Current Weight</span>
         <span className="text-lg font-medium">{curKiloAmount} kg</span>
         <span className="text-lg font-medium">
           {Math.floor(curAmount * 100) / 100} lbs
